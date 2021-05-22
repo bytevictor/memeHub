f23512c661312370090a850d199b07a7a77c5107
bytevictor <victorgonzarg@gmail.com>
2021-05-22 16:23:06 +0200

    Configuración de cypress para cargar estilos. Avanza todas las HUs (hay que testearlas). Con esto último ya se puede empezar a testear bien.

--- src/test/Editor.test.js
+++ src/test/Editor.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { mount } from '@cypress/react';
 import Editor from '../components/editor';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 it('Renders Editor page', () => {
   mount(<Editor />);
