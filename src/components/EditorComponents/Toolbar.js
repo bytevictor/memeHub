de05db049c9c28923a5d86403aa2eda0cf5f4782
bytevictor <victorgonzarg@gmail.com>
2021-06-17 22:19:28 +0200

    Añadidas tooltips a la barra de herramientas. Avanza [HU22] #45

--- src/components/EditorComponents/Toolbar.js
+++ src/components/EditorComponents/Toolbar.js
@@ -6,13 +6,12 @@ import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 import {createMuiTheme} from '@material-ui/core/styles'
 import {ThemeProvider} from '@material-ui/styles'
 
+import Tooltip from '@material-ui/core/Tooltip';
+import Zoom from '@material-ui/core/Zoom';
 import GestureIcon from '@material-ui/icons/Gesture';
-import BrushIcon from '@material-ui/icons/Brush';
 import Crop169Icon from '@material-ui/icons/Crop169';
 import FormatShapesIcon from '@material-ui/icons/FormatShapes';
 import ShowChartIcon from '@material-ui/icons/ShowChart';
-import SelectAllIcon from '@material-ui/icons/SelectAll';
-import TextFormatIcon from '@material-ui/icons/TextFormat';
 import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 
 
@@ -43,21 +42,33 @@ export default function Toolbar(props) {
       className="menuherramientas"
       
     >
+      
       <ToggleButton value="SelectorAndText" aria-label="Selector">
-        <FormatShapesIcon />
+        <Tooltip title="Selector and Text creator" placement="left" TransitionComponent={Zoom} arrow>
+          <FormatShapesIcon/>
+        </Tooltip>
       </ToggleButton>
       <ToggleButton value="FreeLine" aria-label="FreeLine">
-        <GestureIcon />
+        <Tooltip title="Free Line" placement="left" TransitionComponent={Zoom} arrow>
+          <GestureIcon />
+        </Tooltip>
       </ToggleButton>
       <ToggleButton value="StraightLine" aria-label="StraightLine">
-        <ShowChartIcon />
-      </ToggleButton>
+        <Tooltip title="Straight Line" placement="left" TransitionComponent={Zoom} arrow>
+          <ShowChartIcon />
+        </Tooltip>
+      </ToggleButton>    
       <ToggleButton value="Rectangle" aria-label="Rectangle">
+        <Tooltip title="Rectangle" placement="left" TransitionComponent={Zoom} arrow>
         <Crop169Icon />
+        </Tooltip>
       </ToggleButton>
       <ToggleButton value="Ellipse" aria-label="Ellipse">
-        <RadioButtonUncheckedIcon />
+        <Tooltip title="Ellipse" placement="left" TransitionComponent={Zoom} arrow>
+          <RadioButtonUncheckedIcon />
+        </Tooltip>
       </ToggleButton>
+      
     </ToggleButtonGroup>
     </ThemeProvider>
   );
