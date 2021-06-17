07406bf01962390d1419bab0e602c7720f81f3ce
bytevictor <victorgonzarg@gmail.com>
2021-06-18 01:03:26 +0200

    Testea las tooltips de la toolbar. Termina [HU22] closes #45

--- src/test/Tools.test.js
+++ src/test/Tools.test.js
@@ -47,4 +47,22 @@ describe('Toolbar tests', () => {
       })
     })
   })
+  it('should show a tooltip', () => {
+    mount(<Editor />);
+
+    cy.readFile('src/test/mante.png', 'base64').then( (mante) =>{
+      let base_64 = 'data:image/png;base64,' + mante
+      let mante_file = dataURLtoFile(base_64, 'mante.png')
+      let dataTransfer = dataTransferFileObject(mante_file)
+
+      cy.get('.drop-container').trigger('drop', { dataTransfer }).then( () => {
+        cy.wait(1000)
+        cy.waitForReact()
+
+        cy.get('button[aria-label="FreeLine"]').trigger('mouseover').then( () => {
+          cy.get('div[role=tooltip]').should('exist')
+        })
+      })
+    })
+  })
 })
